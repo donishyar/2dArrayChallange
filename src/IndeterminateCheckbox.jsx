c7574394a238f184ac0d1fc983aead0322eb07f7
donishyar <ahmadshahdonishyar@gmail.com>
2024-01-08 00:22:42 +0430

    working with another algorithm

---
+++ src/IndeterminateCheckbox.jsx
@@ -0,0 +1,138 @@
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import "./IndeterminateCheckbox.css";
+import { initialPermissionList, RowNames } from "./utils";
+
+const ParentChildApp = () => {
+  const [permissionList, setPermissionList] = useState(initialPermissionList);
+
+  const handleCheckColumnCheckbox = (row, col) => {
+    const newPermissionList = [...permissionList];
+    newPermissionList[row][col] = !newPermissionList[row][col];
+    setPermissionList(newPermissionList);
+
+    if (permissionList[row][col] === true) {
+      const updatedPermList = [...permissionList];
+      for (let i = 0; i < updatedPermList.length; i++) {
+        for (let j = 0; j < updatedPermList[i].length; j++) {
+          updatedPermList[i][col] = true;
+        }
+      }
+    } else {
+      const updatedPermList = [...permissionList];
+      for (let i = 0; i < updatedPermList.length; i++) {
+        for (let j = 0; j < updatedPermList[i].length; j++) {
+          updatedPermList[i][col] = false;
+        }
+      }
+    }
+  };
+
+  const handleCheckAll = () => {
+    const updatedPermissions = [...permissionList];
+    for (let i = 0; i < initialPermissionList.length; i++) {
+      updatedPermissions[i] = updatedPermissions[i].map((child) => !child);
+      setPermissionList(updatedPermissions);
+    }
+  };
+
+  const handleParentChange = (parentIndex) => {
+    if (parentIndex == 0) {
+      handleCheckAll();
+    } else {
+      const updatedPermissions = [...permissionList];
+      updatedPermissions[parentIndex] = updatedPermissions[parentIndex].map(
+        (child) => !child
+      );
+      setPermissionList(updatedPermissions);
+    }
+  };
+
+  const handleChildChange = (parentIndex, childIndex) => {
+    if (parentIndex == 0) {
+      handleCheckColumnCheckbox(parentIndex, childIndex);
+    } else {
+      const updatedPermissions = [...permissionList];
+      updatedPermissions[parentIndex][childIndex] =
+        !updatedPermissions[parentIndex][childIndex];
+      setPermissionList(updatedPermissions);
+      console.log(
+        "from handleChild function " + parentIndex + " " + childIndex
+      );
+    }
+  };
+
+  const children = (parentIndex) => (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        ml: 3,
+        justifyContent: "flex-end",
+        gap: "1rem",
+      }}
+    >
+      {[...Array(7).keys()].map((childIndex) => (
+        <FormControlLabel
+          key={childIndex}
+          control={
+            <Checkbox
+              checked={permissionList[parentIndex][childIndex]}
+              onChange={() => handleChildChange(parentIndex, childIndex)}
+            />
+          }
+        />
+      ))}
+    </Box>
+  );
+
+  useEffect(() => {}, [initialPermissionList]);
+
+  return (
+    <div className="rowList">
+      {[...permissionList.keys()].map((parentIndex) => (
+        <div
+          key={parentIndex}
+          className={
+            parentIndex === 0
+              ? "parentRow"
+              : parentIndex % 2 === 0
+              ? "Row_odd"
+              : "Row_even"
+          }
+        >
+          <FormControlLabel
+            className={parentIndex == 0 ? "checkAll" : null}
+            label={RowNames[parentIndex]}
+            labelPlacement="start"
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              ml: 3,
+              justifyContent: "center",
+            }}
+            control={
+              <Checkbox
+                checked={permissionList[parentIndex].every((child) => child)}
+                indeterminate={
+                  parentIndex === 0
+                    ? (!permissionList[parentIndex].every((child) => child) &&
+                        permissionList[parentIndex].some((child) => child)) ||
+                      permissionList.some((perm) => perm.some((child) => child))
+                    : !permissionList[parentIndex].every((child) => child) &&
+                      permissionList[parentIndex].some((child) => child)
+                }
+                onChange={() => handleParentChange(parentIndex)}
+              />
+            }
+          />
+          {children(parentIndex)}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ParentChildApp;
--- src/utils.js
+++ src/utils.js
@@ -10,7 +10,31 @@ export const initialPermissionList = [
   [false, false, false, false, false, false, false, false],
 ];
 
-export const RowsList = [
+export const checker = (row,col) => {
+  // eslint-disable-next-line for-direction
+  for(let i = 0 ; i<initialPermissionList.length;i++){
+   for(let j = 0 ; j<initialPermissionList[i]; j++) {
+    if(row == 0 && col == 0){
+      initialPermissionList[i][j] = true;
+      console.log(initialPermissionList[i][j]);
+    }
+     else if(row == 0 && col !== 0) {
+      initialPermissionList[row][j] = true;
+      console.log(initialPermissionList[row][j]);
+    }
+    else if(col == 0 && row !== 0) {
+      initialPermissionList[j][col] = true;
+      console.log(initialPermissionList[j][col]);
+    }else {
+      initialPermissionList[row][col] = true;
+      console.log(initialPermissionList[row][col]);
+
+    }
+   }
+  }
+}
+
+export const ColumnList = [
   {
     id: 1,
     name: "List",
@@ -48,47 +72,54 @@ export const RowsList = [
   },
 ];
 
-export const ColumnList = [
+export const RowsList = [
   {
     id: 0,
-    name: "DashBoard",
+    name: "Check All",
     isNull: false,
   },
   {
     id: 1,
-    name: "Storage",
+    name: "DashBoard",
     isNull: false,
   },
   {
     id: 2,
-    name: "Custom Provider",
+    name: "Storage",
     isNull: false,
   },
   {
     id: 3,
-    name: "Stock Out",
+    name: "Custom Provider",
     isNull: false,
   },
   {
     id: 4,
-    name: "Invoice",
+    name: "Stock Out",
     isNull: false,
   },
   {
     id: 5,
-    name: "Unloading",
+    name: "Invoice",
     isNull: false,
   },
   {
     id: 6,
-    name: "User",
+    name: "Unloading",
     isNull: false,
   },
   {
     id: 7,
+    name: "User",
+    isNull: false,
+  },
+  {
+    id: 8,
     name: "Role",
     isNull: false,
   },
 ];
 
 
+export const RowNames = RowsList.map(row => row.name);
+export const ColumnNames = ColumnList.map(row => row.name);
