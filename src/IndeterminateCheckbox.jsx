d003135d2b4becdab5bcacbcc02200197c0401fc
donishyar <ahmadshahdonishyar@gmail.com>
2024-01-09 22:31:59 +0430

    working on columnCheck indeterminate

--- src/IndeterminateCheckbox.jsx
+++ src/IndeterminateCheckbox.jsx
@@ -3,19 +3,20 @@ import Box from "@mui/material/Box";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import "./IndeterminateCheckbox.css";
-import {initialPermissionList, RowNames } from "./utils";
+import { ColumnList, initialPermissionList, RowNames } from "./utils";
 
 const ParentChildApp = () => {
   const [permissionList, setPermissionList] = useState(initialPermissionList);
   const [isChecked, setIsChecked] = useState(true);
-  const [indeterminate,setIndeterminate] = useState(true);
+  const [isChecked2, setIsChecked2] = useState(true);
+  const [indeterminate, setIndeterminate] = useState(true);
+  const [columnNullChecker, setColumnNullChecker] = useState(false);
 
   const handleIndeterminateChange = (parentIndex) => {
     const updatedPermissions = [...permissionList];
-    updatedPermissions[parentIndex] = updatedPermissions[parentIndex].map(
-      () => setIndeterminate(!indeterminate)
+    updatedPermissions[parentIndex] = updatedPermissions[parentIndex].map(() =>
+      setIndeterminate(indeterminate)
     );
-
     setPermissionList(updatedPermissions);
   };
 
@@ -49,7 +50,7 @@ const ParentChildApp = () => {
 
     // Set all values in permissionList based on the current state
     const updatedPermissions = permissionList.map((permission) =>
-      permission.map(() => isChecked),
+      permission.map(() => isChecked)
     );
 
     // Update the permissionList state
@@ -57,37 +58,61 @@ const ParentChildApp = () => {
   };
 
   const handleParentChange = (parentIndex) => {
+    handleRowsNullChecker();
     if (parentIndex == 0) {
       handleCheckAll();
+      setIndeterminate(!indeterminate);
     } else {
+      setIsChecked2((prevChecked) => !prevChecked);
       const updatedPermissions = [...permissionList];
       updatedPermissions[parentIndex] = updatedPermissions[parentIndex].map(
-        (child) => !child,
+        (child) => isChecked2
       );
       setPermissionList(updatedPermissions);
     }
   };
 
   const handleChildChange = (parentIndex, childIndex) => {
+    handleRowsNullChecker();
     if (parentIndex == 0) {
       handleCheckColumnCheckbox(parentIndex, childIndex);
+      handleRowsNullChecker(parentIndex, childIndex);
     } else {
       const updatedPermissions = [...permissionList];
       updatedPermissions[parentIndex][childIndex] =
         !updatedPermissions[parentIndex][childIndex];
       setPermissionList(updatedPermissions);
-      // if(ColumnList.some((col) => col.id === childIndex+1)){
-      //   ColumnList.forEach((col)=> {
-      //     if(col.id === childIndex+1){
-      //       col.isNull = true;
-      //     }else{
-      //       col.isNull = false;
-      //     }
-      //   })
-      // }
+      if (ColumnList.some((col) => col.id === childIndex + 1)) {
+        ColumnList.forEach((col) => {
+          if (col.id === childIndex + 1) {
+            col.isNull = true;
+          } else {
+            col.isNull = false;
+          }
+        });
+      }
       console.log(
-        "from handleChild function " + parentIndex + " " + childIndex,
+        "from handleChild function " + parentIndex + " " + childIndex
       );
+    }
+  };
+
+  const handleRowsNullChecker = () => {
+    for (let j = 0; j < initialPermissionList[0].length; j++) {
+      let hasTrue = false;
+
+      for (let i = 0; i < initialPermissionList.length; i++) {
+        if (initialPermissionList[i][j] === true) {
+          hasTrue = true;
+          break; // Exit the loop if a true value is found in the column
+        }
+      }
+
+      ColumnList.forEach((col) => {
+        if (col.id === j + 1) {
+          col.isNull = hasTrue;
+        }
+      });
     }
   };
 
@@ -106,13 +131,7 @@ const ParentChildApp = () => {
           key={childIndex}
           control={
             <Checkbox
-              // indeterminate={
-              //   parentIndex === 0
-              //     ? ColumnList.some(
-              //         (col) => col.isNull === true && col.id === childIndex + 1,
-              //       )
-              //     : null
-              // }
+              indeterminate={parentIndex === 0 && ColumnList[childIndex].isNull}
               checked={permissionList[parentIndex][childIndex]}
               onChange={() => handleChildChange(parentIndex, childIndex)}
             />
@@ -122,7 +141,7 @@ const ParentChildApp = () => {
     </Box>
   );
 
-  useEffect(() => {}, [initialPermissionList]);
+  useEffect(() => {}, [initialPermissionList, ColumnList]);
 
   return (
     <div className="rowList">
@@ -133,8 +152,8 @@ const ParentChildApp = () => {
             parentIndex === 0
               ? "parentRow"
               : parentIndex % 2 === 0
-                ? "Row_odd"
-                : "Row_even"
+              ? "Row_odd"
+              : "Row_even"
           }
         >
           <FormControlLabel
@@ -155,7 +174,7 @@ const ParentChildApp = () => {
                     ? (!permissionList[parentIndex].every((child) => child) &&
                         permissionList[parentIndex].some((child) => child)) ||
                       permissionList.some((perm) => perm.some((child) => child))
-                    : !permissionList[parentIndex].every((child) => child)  &&
+                    : !permissionList[parentIndex].every((child) => child) &&
                       permissionList[parentIndex].some((child) => child)
                 }
                 onChange={() => handleParentChange(parentIndex)}
